581f250004984a1f14cda60057801e1304b5855f
Eric Teubert <eric@ericteubert.de>
2014-01-04 11:42:56 +0100

    init

---
+++ lib/Podlove/Comment/Comment.php
@@ -0,0 +1,6 @@
+<?php 
+namespace Podlove\Comment;
+
+class Comment {
+	
+}
---
+++ test/CommentTest.php
@@ -0,0 +1,10 @@
+<?php
+use Podlove\Comment\Comment;
+
+class CommentTest extends PHPUnit_Framework_TestCase {
+
+	public function testHelloWorld() {
+		$this->assertEquals( "Hi", "Hi" );
+	}
+
+}
